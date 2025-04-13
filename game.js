bcf91ba6d487d2940d50317fbf7cf7cf3a244913
Feng-03 <lefengnoblade@gmail.com>
2025-04-12 18:57:06 -0700

    Add tool shit
    Co-authored-by: idontknowicantknow <idontknowicantknow@users.noreply.github.com>

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
-import {createElem, bound} from "./randomStuff.js";
+import {createElem, bound, polarToCartesian, dist} from "./randomStuff.js";
 
 // global variable spam cuz they dont pay me enough
-const tileSize = 50;
-const mapWidth = 10;
-const mapHeight = 10;
+export const tileSize = 50;
+export const mapWidth = 10;
+export const mapHeight = 10;
 
 class Entity {
     /**
@@ -13,23 +13,27 @@ class Entity {
      * @param speed Speed in tiles / frame
      * @param imgSrc Path to this entity's image
      */
-    constructor(x, y, health, speed, imgSrc) {
-        this.oldX = x;
-        this.oldY = y;
-        this.oldRot = 0;
+    constructor(x, y, health, speed, imgSrc, parentElem) {
+        // move them comments into jsdoc
+        this.oldX = x; // x coordinate from previous frame, used to update image only when necessary
+        this.oldY = y; // old y coordinate
+        this.oldRotation = 0; // old rotation
         this.x = x;
         this.y = y;
-        this.rot = 0;
+        this.rotation = 0; // rotation in radians
         this.health = health;
         this.speed = speed;
         this.image = createElem("img", {src: imgSrc, width: tileSize}, {position: "absolute", left: "0", top: "0"});
+        this.parentElem = parentElem;
+        this.parentElem.appendChild(this.image);
     }
 
     update() {
         this.x = bound(this.x, 0, mapWidth);
         this.y = bound(this.y, 0, mapHeight);
-        if (this.oldX !== this.x || this.oldY !== this.y || this.oldRot !== this.rot) {
-            this.image.style.transform = `translate(${this.x * tileSize}px, ${this.y * tileSize}px) rotate(${this.rot}rad)`;
+        // angle threshold arbitrarily chosen
+        if (this.oldX !== this.x || this.oldY !== this.y || angleDiff(this.rotation, this.oldRotation) > Math.PI / 16) {
+            this.image.style.transform = `translate(${this.x * tileSize}px, ${this.y * tileSize}px) rotate(${this.rotation}rad)`;
         }
     }
 
@@ -38,23 +42,84 @@ class Entity {
     }
 }
 
-class Player extends Entity {
-    constructor() {
+export class Player extends Entity {
+    constructor(tool) {
+        this.tool = tool;
         super();
         // they dont pay me enough
-        addEventListener("keydown", event => {
-            if (event.key === "w") {
-                this.y -= this.speed;
-            }
-            if (event.key === "s") {
-                this.y += this.speed;
-            }
-            if (event.key === "a") {
-                this.x -= this.speed;
-            }
-            if (event.key === "d") {
-                this.y += this.speed;
-            }
+        keys = new Map();
+        this.parentElem.addEventListener("keydown", event => {
+            this.keys.set(event.key, true)
         });
+        this.parentElem.addEventListener("keyup", event => {
+            this.keys.set(event.key, false);
+        });
+        // im sure throwing this shit in an event listener will have absolutely no problems with rotation change detection in update()
+        this.parentElem.addEventListener("mousemove", event => {
+            this.tool.rotation = Math.atan2(event.pageY / tileSize - this.y, event.pageX / tileSize - this.x);
+        });
+    }
+
+    update() {
+        if (this.keys.get("w")) {
+            this.y -= this.speed;
+        }
+        if (this.keys.get("s")) {
+            this.y += this.speed;
+        }
+        if (this.keys.get("a")) {
+            this.x -= this.speed;
+        }
+        if (this.keys.get("d")) {
+            this.y += this.speed;
+        }
+        super.update();
+    }
+
+    die() {
+        removeEventListener("keydown", this.handleKeyboard)
+    }
+}
+
+class Tool extends Entity {
+    constructor() {
+        super();
+        this.shit = [];
+        this.maxCapacity = -69;
+    }
+
+    grabShit(worldShit) {
+        throw new Error("override this you idiot")
+    }
+
+    depositShit() {
+        for (const shit of this.shit) {
+            shit.oof();
+        }
+        this.shit = [];
+    }
+}
+
+export class Net extends Tool {
+    maxCapacity = 1
+    constructor(grabberLength, grabRadius) {
+        this.grabberLength = grabberLength;
+        this.grabRadius = grabRadius;
+    }
+
+    grabShit(worldShit) {
+        if (this.shit.length >= this.maxCapacity) {
+            return;
+        }
+        const netPos = polarToCartesian(this.grabberLength, this.rotation);
+        netPos[0] += this.x;
+        netPos[1] += this.y;
+        for (const shit of worldShit) {
+            if (dist(netPos[0], netPos[1], shit.x, shit.y) <= this.grabRadius) {
+                this.shit.push(shit)
+                shit.die();
+                return;
+            }
+        }
     }
 }
--- randomStuff.js
+++ randomStuff.js
@@ -25,3 +25,36 @@ export function createElem(type, properties = {}, styles = {}, ...children) {
 export function bound(value, min, max) {
     return Math.max(Math.min(value, max), min);
 }
+
+/**
+ * Polar coordinates to cartesian coordinates
+ * @param r Radius
+ * @param theta Angle in radians, including coterminal
+ * @returns Cartesian [x, y]
+ */
+export function polarToCartesian(radius, rotation) {
+    return [Math.cos(theta) * r, Math.sin(theta) * r];
+}
+
+/**
+ * Distance between two cartesian points using pythagorean theorems.
+ * @param x1 X coordinate of first point
+ * @param y1 Y coordinate of first point
+ * @param x2 X coordinate of second point
+ * @param y2 Y coordinate of second point
+ * @returns Distance
+ */
+export function dist(x1, y1, x2, y2) {
+    return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+}
+
+/**
+ * Angle between two radian points. May handle negatives. Will not handle coterminal angles.
+ * @param one First radian angle
+ * @param two Second radian angle
+ * @returns Possibly negative difference in radians
+ */
+export function angleDiff(one, two) {
+    // dont ask me what this is
+    return mod((one - two) + Math.PI, 2 * Math.PI) - Math.PI;
+}
